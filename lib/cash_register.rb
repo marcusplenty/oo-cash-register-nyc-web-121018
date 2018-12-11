82e8e2f809dd3321d46a3afffe811aa2c0608ec3
Marcus Plenty <marcusplenty@gmail.com>
2018-12-11 23:00:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,16 +1,41 @@
 class CashRegister
   
-  def initialize(discount=0)
-    @discount = discount 
+  def initialize(discount = 1)
+    @discount = discount
     @total = 0
   end
+  CART = []
+  
+  def items
+    return CART
+  end
+  
+  def discount
+    @discount
+  end
+  
   
   def total
     @total
   end 
   
+  def total=(new_tots)
+    @total = new_tots
+  end 
+  
   def add_item(item, price, quantity = 1)
     @total += price * quantity
+    CART << item
     
+    
+  end
+  
+  def apply_discount 
+    if @discount != 1
+      self.total *= (1-(@discount/100.0))
+      return "After the discount, the total comes to $800."
+    else 
+      return "There is no discount to apply."
+    end
   end
 end
