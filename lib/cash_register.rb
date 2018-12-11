33123a6f4f8fd94a26a1d192b1aa1396af07f1b9
Marcus Plenty <marcusplenty@gmail.com>
2018-12-11 22:47:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,16 +1,30 @@
 class CashRegister
   
-  def initialize(discount=0)
-    @discount = discount 
+  def initialize(discount = 1)
+    @discount = discount
     @total = 0
   end
+  
+  def discount
+    @discount
+  end
+  
   
   def total
     @total
   end 
   
+  def total=(new_tots)
+    @total = new_tots
+  end 
+  
   def add_item(item, price, quantity = 1)
     @total += price * quantity
     
+  end
+  
+  def apply_discount 
+    yep = (100 - @discount)/100
+    self.total = self.total *yep
   end
 end
