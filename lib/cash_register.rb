a1cd4978ee37d87945c27d097ed3e12bfbdbbbdb
Marcus Plenty <marcusplenty@gmail.com>
2018-12-11 21:54:30 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
-
+class CashRegister
+  
+  def initialize(discount=0)
+    @discount = discount 
+    @total = 0
+  end
+  
+  def total
+    @total
+  end 
+  
+  def add_item(item, price, quantity = 1)
+    @total += price * quantity
+    
+  end
+end
